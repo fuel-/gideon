476b49cc62014d99189745352e909a6fd35be381
Jasen Nicely <jasen.nicely@sap.com>
2018-08-31 12:20:50 -0500

    wip

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/fuel-/gideon/bot"
 	"github.com/fuel-/gideon/config"
 )
 
 func main() {
-
+	fmt.Println(os.Getwd())
 	err := config.ReadConfig()
 
 	if err != nil {
